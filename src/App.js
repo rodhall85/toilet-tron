7f3ef5f5f867088093cc30e9a94b17105a3d7583
Rod Hall <rodhall@hotmail.co.uk>
2018-12-07 16:34:25 +0000

    Create the ToiletTron 5000

--- src/App.js
+++ src/App.js
@@ -1,28 +1,96 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import ReactHtmlParser from 'react-html-parser';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      info: 'ToileTron',
+      className: 'App-logo',
+      action: '',
+    }
+
+    
+
+    const child = <div>5000</div>
+    this.stuff = {
+      className: "bob",
+      children: child
+    }
+
+
+  }
+
+  componentDidMount() {
+    // make api reqest to http://api.icndb.com/jokes/random\?firstName\=Toilet
+    // update state.joke
+    // display state.joke
+    this.loadJoke();
+  }
+
+  loadJoke = async () => {
+    const response = await axios({
+    url: 'http://api.icndb.com/jokes/random/firstName/Toilet'
+     });
+
+    this.props.dispatch({type:'updateJoke', payload:response.data.value.joke});
+  }
+
+  changeSize = (size) => {
+    
+    const classSizeName = `App-logo-${size}`;
+    this.setState({
+      ...this.state,
+      className: `App-logo ${classSizeName}`
+    })
+  }
+
+  submitStuff = (event) => {
+    event.preventDefault();
+    this.loadJoke();
+    this.setState({
+      ...this.state,
+      className: `App-logo-${this.state.action}`
+    })
+
+  }
+
+  handleChange = (event) => {
+    const {value} = event.target;
+    this.setState({
+      ...this.state,
+      action: value
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className={this.state.className} alt="logo"  onMouseEnter={() => this.changeSize('large')} onMouseLeave={() => this.changeSize('small')} />
+          <div>{this.state.info}</div>
+          <div>{ReactHtmlParser(this.props.joke)}</div>
+          <form onSubmit={this.submitStuff}>
+            <input type="text" value={this.state.action} onChange={this.handleChange}/>
+            <button type="submit" disabled={["flush", "unflush"].indexOf(this.state.action) === -1}>Submit</button>
+          </form>
         </header>
       </div>
     );
   }
 }
+
+
+
+
+
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider, connect } from 'react-redux';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const jokeStoreReducer = (state = {joke:'cue laughs'}, action) => {
+    const newState = {
+        ...state,
+        joke: action.payload
+    }
+    return newState;
+}
+
+const store = createStore(jokeStoreReducer);
+
+const AppWrapped = connect(state => { return { joke: state.joke }})(App);
+
+ReactDOM.render(<Provider store={store}><AppWrapped /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
